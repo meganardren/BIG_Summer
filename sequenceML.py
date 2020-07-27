e2513dfc4e3a70d0ce295f554152afdc1d3d41b1
meganardren <54588338+meganardren@users.noreply.github.com>
2020-07-27 17:11:24 -0400

    Update sequenceML.py

--- sequenceML.py
+++ sequenceML.py
@@ -38,6 +38,27 @@ def sequence_file(tile_num):
     #create data dataframe
     data = pd.concat([conv_15, gkm_15, deepfact_15, kmer_15, sharpr_15], axis=1)
 
+    #check correlation
+    sharpr_15_array = np.asarray(sharpr_15)
+    sharpr_15_array = sharpr_15_array.flatten()
+    conv_15_array = np.asarray(conv_15)
+    conv_15_array = conv_15_array.flatten()
+    deepfact_15_array = np.asarray(deepfact_15)
+    deepfact_15_array = deepfact_15_array.flatten()
+    kmer_15_array = np.asarray(kmer_15)
+    kmer_15_array = kmer_15_array.flatten()
+    gkm_15_array = np.asarray(gkm_15)
+    gkm_15_array = gkm_15_array.flatten()
+    print('corr of SHARPR score and dragoNN ConvModel:')
+    print(pd.Series(sharpr_15_array).corr(pd.Series(conv_15_array)))
+    print('corr of SHARPR score and gkmSVMModel:')
+    print(pd.Series(sharpr_15_array).corr(pd.Series(gkm_15_array)))
+    print('corr of SHARPR score and dragoNN DeepFactorizedModel:')
+    print(pd.Series(sharpr_15_array).corr(pd.Series(deepfact_15_array)))
+    print('corr of SHARPR score and 6merModel:')
+    print(pd.Series(sharpr_15_array).corr(pd.Series(kmer_15_array)))
+    
+
     #reset index to be able to access region_id
     data = data.reset_index()
 
@@ -51,7 +72,8 @@ def sequence_file(tile_num):
     #dropping the chrom column
     test_set = test_set.drop(columns=['chrom'])
     train_set = train_set.drop(columns=['chrom'])
-    
+    data = data.drop(columns=['chrom'])
+
     #format training data
     labels = train_set['sharpr_%s'%tile_num].values
     train_set = train_set.drop(columns=['region_id','sharpr_%s'%tile_num])
@@ -61,7 +83,7 @@ def sequence_file(tile_num):
     encodings_df.fillna(encodings_df.mean(), inplace=True)
     encodings = encodings_df.to_numpy()
     labels = np.reshape(labels,(15537,))
-    
+
     #format the testing data
     test_labels = test_set['sharpr_%s'%tile_num].values
     test_set = test_set.drop(columns=['region_id','sharpr_%s'%tile_num])
@@ -71,65 +93,66 @@ def sequence_file(tile_num):
     test_encodings_df.fillna(test_encodings_df.mean(), inplace=True)
     test_encodings = test_encodings_df.to_numpy()
     test_labels = np.reshape(test_labels,(183,))
-    
+
+
+    #format data
+    data_labels = data['sharpr_%s'%tile_num].values
+    data = data.drop(columns=['region_id','sharpr_%s'%tile_num])
+    data_encodings = data.values.tolist()
+    data_encodings = np.asarray(data_encodings)
+    data_encodings_df = pd.DataFrame(data_encodings)
+    data_encodings_df.fillna(data_encodings_df.mean(), inplace=True)
+    data_encodings = data_encodings_df.to_numpy()
+    data_labels = np.reshape(data_labels,(15720,))
+
     #train a linear regressor on the training dataset. 
     from sklearn.linear_model import LinearRegression
 
     sequence_regressor = LinearRegression()
     sequence_regressor.fit(encodings, labels)
-    
+
     #compute mse
+    #     from sklearn.metrics import mean_squared_error
+    #     test_predictions = sequence_regressor.predict(test_encodings)
+    #     test_mse = mean_squared_error(predictions, data_labels)
+    #     test_rmse = np.sqrt(test_mse)
     from sklearn.metrics import mean_squared_error
-    test_predictions = sequence_regressor.predict(test_encodings)
-    test_mse = mean_squared_error(test_predictions, test_labels)
-    test_rmse = np.sqrt(test_mse)
-    
-    test_encodings=test_encodings.flatten()
-    test__labels=test_labels.flatten()
+    predictions = sequence_regressor.predict(data_encodings)
+    mse = mean_squared_error(predictions, data_labels)
+    rmse = np.sqrt(mse)
+
+    data_encodings=data_encodings.flatten()
+    data__labels=data_labels.flatten()
     #visualize the results
-    corr = pd.Series(test_predictions).corr(pd.Series(test_labels))
+    corr = pd.Series(predictions).corr(pd.Series(data_labels))
+
     caption = "correlation pearson r= %s"%(corr)
-    rmse = "root mean squared error = %s"%test_rmse
+    rmse = "root mean squared error = %s"%rmse
 
 
-#     plt.scatter(
-#         x=test_predictions,
-#         y=test_labels,
-#         edgecolors='w'
-#     )
-#     tile_num = int(tile_num)
-#     tile_num = tile_num + 1
+    plt.scatter(
+        x=predictions,
+        y=data_labels,
+        edgecolors='w'
+    )
+    tile_num = int(tile_num)
+    tile_num = tile_num + 1
 
-#     plt.title("Tile %s chr 21 Sequence Regressor"%(tile_num))
-#     plt.xlabel("test_predictions \n\n %s \n\n %s"%(caption,rmse))
-#     plt.ylabel("test_labels")
+    plt.title("Tile %s Sequence Regressor"%(tile_num))
+    plt.xlabel("test_predictions \n\n %s \n\n %s"%(caption,rmse))
+    plt.ylabel("test_labels")
 
-#     plt.savefig("sequence_predictions_tile%s"%tile_num)
-    
+    plt.savefig("sequence_predictions_tile%s"%tile_num)
+
+    print("Regressor coefficients(ConvModel,gkmSVMModel,DeepFactorizedModel,6merModel):")
+    print(sequence_regressor.coef_)
+
 #     fn = open("sequence_predictions_tile%s.txt"%tile_num,"w")
-#     np.savetxt(fn,test_predictions)
+#     np.savetxt(fn,predictions)
 #     fn.close()
 
-    return corr
 
 if __name__ == "__main__":
     # take tile_num in as a parameter
-    #tile_num = sys.argv[1]
-    corr = []
-    for tile_num in range(30):
-        tile_num = str(tile_num)
-        corr_tile = sequence_file(tile_num)
-        corr.append(corr_tile)
-    
-    
-    tiles = list(range(1, 31))
-     plt.plot(
-        tiles,
-        corr
-    )
-
-    plt.title("Correlation by Tile")
-    plt.xlabel("tile")
-    plt.ylabel("correlation")
-
-    plt.show("sequence_predictions_tile")
+    tile_num = '15'
+    sequence_file(tile_num)
