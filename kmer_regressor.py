112f0849b30699a2b62c9df857cea67b39ef8c49
meganardren <54588338+meganardren@users.noreply.github.com>
2020-07-30 11:47:31 -0400

    Update kmer_regressor.py

--- kmer_regressor.py
+++ kmer_regressor.py
@@ -9,11 +9,11 @@ def kmer_regressor(tile_num):
     kmer_size = '6mer'
     Data_path = "/u/home/m/mardren/scratch/KmerML"
     #read in data
-    sequence_label = pd.read_pickle('%s/%s_sequences_label.pkl'%(Data_path, kmer_size))
-    sequence_encoding = pd.read_pickle('%s/%s_sequences_encoding.pkl'%(Data_path, kmer_size))
-    #subset by selected tile
-    label_tile = sequence_label[[tile_num]]
-    encoding_tile = sequence_encoding[[tile_num]]
+    label_tile = pd.read_pickle('%s/%s_sequences_label.pkl'%(Data_path, kmer_size))
+    encoding_tile = pd.read_pickle('%s/%s_sequences_encoding.pkl'%(Data_path, kmer_size))
+#     #subset by selected tile
+#     label_tile = sequence_label[[tile_num]]
+#     encoding_tile = sequence_encoding[[tile_num]]
     #reformat data
     label_tile =label_tile.unstack().reset_index()
     label_tile = label_tile.rename(columns={0:'label'})
@@ -22,28 +22,33 @@ def kmer_regressor(tile_num):
     encoding_tile = encoding_tile.set_index('region_id')
     label_tile = label_tile.set_index('region_id')
     encoding_tile = encoding_tile.drop(columns=['tile_num'])
-    data = pd.concat([label_tile,encoding_tile], axis=1)
-    data = data.reset_index()
-    data['chrom'] = data['region_id'].str.split('_').str[3]
+    all_data = pd.concat([label_tile,encoding_tile], axis=1)
+    all_data = all_data.reset_index()
+    all_data['chrom'] = all_data['region_id'].str.split('_').str[3]
 
-    #setting aside chr21for testing data
-    testing_data_chr21 = data[data["chrom"] == 'chr21']
-    #removing chr21 from training data
-    training_data_nochr21 = data[data["chrom"] != 'chr21']
-    #dropping the chrom column
-    test_set = testing_data_chr21.drop(columns=['chrom'])
-    train_set = training_data_nochr21.drop(columns=['chrom'])
+    #setting aside chr1 and chr2 for testing data
+    train_chroms = [f'chr{i}' for i in range(1,23,2)]
+    test_chroms = [f'chr{i}' for i in range(2,23,2)]
+    chrX = all_data[all_data["chrom"]==('chrX')]
+    test_set = all_data[all_data["chrom"].isin(test_chroms)]
+    train_set = all_data[all_data["chrom"].isin(train_chroms)]
+    train_set = train_set.append(chrX)
+
+    test_set = test_set.drop(columns=['chrom'])
+    train_set = train_set.drop(columns=['chrom'])
+    all_data = all_data.drop(columns=['chrom'])
+    
 
     #format the training data
     labels = train_set['label'].values
     encodings = train_set['encoding'].values.tolist()
     encodings = np.asarray(encodings)
-    labels = np.reshape(labels,(15537,))
+    labels = np.reshape(labels,(258633 ,))
     #format the testing data
     test_labels = test_set['label'].values
     test_encodings = test_set['encoding'].values.tolist()
     test_encodings = np.asarray(test_encodings)
-    test_labels = np.reshape(test_labels,(183,))
+    test_labels = np.reshape(test_labels,(228687,))
 
     encodings_df = pd.DataFrame(encodings)
     encodings_df.fillna(encodings_df.mean(), inplace=True)
@@ -51,7 +56,7 @@ def kmer_regressor(tile_num):
     labels_df = pd.DataFrame(labels)
     labels_df.fillna(labels_df.mean(), inplace=True)
     labels = labels_df.to_numpy()
-    labels = np.reshape(labels,(15537,))
+    labels = np.reshape(labels,(258633 ,))
 
     test_encodings_df = pd.DataFrame(test_encodings)
     test_encodings_df.fillna(test_encodings_df.mean(), inplace=True)
@@ -59,13 +64,13 @@ def kmer_regressor(tile_num):
     test_labels_df = pd.DataFrame(test_labels)
     test_labels_df.fillna(test_labels_df.mean(), inplace=True)
     test_labels = test_labels_df.to_numpy()
-    test_labels = np.reshape(test_labels,(183,))
+    test_labels = np.reshape(test_labels,(228687,))
 
     #format all data
-    data_labels = data['label'].values
-    data_encodings = data['encoding'].values.tolist()
+    data_labels = all_data['label'].values
+    data_encodings = all_data['encoding'].values.tolist()
     data_encodings = np.asarray(data_encodings)
-    data_labels = np.reshape(data_labels,(15720,))
+    data_labels = np.reshape(data_labels,(487320,))
 
     data_encodings_df = pd.DataFrame(data_encodings)
     data_encodings_df.fillna(data_encodings_df.mean(), inplace=True)
@@ -74,7 +79,7 @@ def kmer_regressor(tile_num):
     data_labels_df.fillna(data_labels_df.mean(), inplace=True)
     data_labels = data_labels_df.to_numpy()
 
-    data_labels = np.reshape(data_labels,(15720,))
+    data_labels = np.reshape(data_labels,(487320,))
 
     #train a linear regressor on the training dataset. 
     from sklearn.linear_model import LinearRegression
@@ -96,26 +101,26 @@ def kmer_regressor(tile_num):
         edgecolors='w'
     )
 
-    plt.title("%s Regressor"%kmer_size)
+    plt.title("%s Regressor on Even testing data"%kmer_size)
     plt.xlabel("test_predictions \n\n %s"%caption)
     plt.ylabel("test_labels")
 
-    plt.show()
+    plt.savefig('6mer_eventesting')
 
     #get predictions for the whole tile
-    # predictions = k_mer_regressor.predict(data_encodings)
-    data['prediction'] = predictions
+    predictions = k_mer_regressor.predict(data_encodings)
+    all_data['prediction'] = predictions
     #save results to use in sequence model
-    data = data.drop(columns=['chrom','encoding'])
+    all_data = all_data.drop(columns=['encoding'])
     #split into kmer_prediction pkl and sharpr_score pkl
-    kmer_prediction = data.drop(columns=['label'])
+    kmer_prediction = all_data.drop(columns=['label'])
     kmer_prediction = kmer_prediction.pivot(index='region_id', columns='tile_num', values='prediction')
-    sharpr_score = data.drop(columns=['prediction'])
+    sharpr_score = all_data.drop(columns=['prediction'])
     sharpr_score = sharpr_score.pivot(index='region_id', columns='tile_num', values='label')
     tile_num = tile_num+1
-    kmer_prediction.to_pickle('%s/%s_prediction_tile%s.pkl'%(Data_path, kmer_size,tile_num))
-    sharpr_score.to_pickle('%s/%s_sharpr_score_tile%s.pkl'%(Data_path, kmer_size,tile_num))
+    kmer_prediction.to_pickle('%s/%s_prediction_tile_evenchrtesting.pkl'%(Data_path, kmer_size))
+    sharpr_score.to_pickle('%s/%s_sharpr_score_tile_evenchrtesting.pkl'%(Data_path, kmer_size))
 
 if __name__ == "__main__":
-    tile_num = 0
+    tile_num = 15
     kmer_regressor(tile_num)
